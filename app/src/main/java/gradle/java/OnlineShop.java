15674cdf50e38bde4489871485e2e9beb7779390
Matias Nabarro <matiasnabarro@gmail.com>
2022-10-19 16:04:30 +0200

    refactor/ deleted ProductsChoice class

--- app/src/main/java/gradle/java/OnlineShop.java
+++ app/src/main/java/gradle/java/OnlineShop.java
@@ -43,4 +43,26 @@ public class OnlineShop {
 
         } else System.out.println("Sorry, you have to choose one valid option");
     }
+
+    public void showProductsToChoose() {
+        String menuOption;
+        ArrayList<Product> catalogue = database.findAll();
+
+        System.out.println("Which product would you like to explore?");
+        for (Product product: catalogue) {
+            menuOption = product.showId() + " - " + product.showImage();
+            System.out.println(menuOption);
+        }
+
+        Scanner myObj = new Scanner(System.in);
+        String selection = myObj.nextLine();
+
+        if (selection.equals(catalogue.get(0).showId())) {
+            System.out.println("DESCRIPTION :" +catalogue.get(0).showFeaturedAttribute());
+        } else if (selection.equals(catalogue.get(1).showId())) {
+            System.out.println("DESCRIPTION :" + catalogue.get(1).showFeaturedAttribute());
+        } else {
+            System.out.println("Sorry, the product doesn't exist");
+        }
+    }
 }
