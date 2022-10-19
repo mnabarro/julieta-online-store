0efd261019aa18cd107cf7d2664812588541f366
Matias Nabarro <matiasnabarro@gmail.com>
2022-10-19 16:09:33 +0200

    refactor/ deleted ProductToChoose instance from CatalogFormatter. Modified Product attributes accesses.

--- app/src/main/java/gradle/java/infraestructure/presentation/CatalogFormatter.java
+++ app/src/main/java/gradle/java/infraestructure/presentation/CatalogFormatter.java
@@ -4,18 +4,15 @@ import gradle.java.domain.Product;
 import java.util.ArrayList;
 
 public class CatalogFormatter {
-    ProductsChoice productsChoice = new ProductsChoice();
-
     public String formattedCatalog(ArrayList<Product> catalog){
         for (Product product : catalog){
-            System.out.println(product.showImage());
-            System.out.println(product.showDescription());
-            System.out.println(product.showFeaturedAttribute());
-            System.out.println(product.showPrice());
-            System.out.println(product.showReference());
+            System.out.println(product.image);
+            System.out.println(product.description);
+            System.out.println(product.featuredAttribute);
+            System.out.println(product.price);
+            System.out.println(product.reference);
             System.out.println("\n");
         }
-        productsChoice.showProductsToChoose();
         return "";
     }
 }
