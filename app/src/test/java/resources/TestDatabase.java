a73f4090dc28c3a4c3028a8a127826b05eab10a7
Matias Nabarro <matiasnabarro@gmail.com>
2022-10-21 17:19:04 +0200

    test / created test database, moved large string literals to a static class

---
+++ app/src/test/java/resources/TestDatabase.java
@@ -0,0 +1,46 @@
+package resources;
+
+import gradle.java.domain.Product;
+import gradle.java.domain.ProductRepository;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Optional;
+
+public class TestDatabase implements ProductRepository {
+
+  private final ArrayList<Product> productList = new ArrayList<>(Arrays.asList(
+    new Product(
+      "\uD83D\uDCFA",
+      "Glamorous panoramic television",
+      "13 inches",
+      """
+        With this panoramic television, your friday nights will be boring no more.
+        The screen is composed of 14 million pixels.
+        Thanks to its low energy consumption design, your bills will stay low.""",
+      300.99,
+      "W2C"),
+    new Product(
+      "\uD83C\uDFB9",
+      "Untuned musical keyboard",
+      "4 octaves",
+      "Tired of your noisy neighbour? Play this untuned musical keyboard for two hours at home and your " +
+        "neighbour will be ready to move to a building far away from you.",
+      1003.00,
+      "X4A"))
+  );
+
+  @Override
+  public ArrayList<Product> findAll() {
+    return productList;
+  }
+
+  public Optional<Product> findByReference(String reference) {
+    for (Product product : productList) {
+      if (Objects.equals(product.reference, reference)) {
+        return Optional.of(product);
+      }
+    }
+    return Optional.empty();
+  }
+}
---
+++ app/src/test/java/resources/TestStrings.java
@@ -0,0 +1,27 @@
+package resources;
+
+public class TestStrings {
+
+  public static final String expectedFormattedProductDetail = """
+    \uD83D\uDCFA
+    Reference: W2C
+    Price: 300.99 €
+    12 left
+    SUMMARY:
+    Glamorous panoramic television, 13 inches
+
+    DESCRIPTION:
+    With this panoramic television, your friday nights will be boring no more.
+    The screen is composed of 14 million pixels.
+    Thanks to its low energy consumption design, your bills will stay low.
+
+    """;
+
+  public static final String expectedFormattedProduct = """
+    \uD83C\uDFB9
+    Untuned musical keyboard, 4 octaves
+    Price: 1003.0 €
+    Reference: X4A
+    """;
+
+}
