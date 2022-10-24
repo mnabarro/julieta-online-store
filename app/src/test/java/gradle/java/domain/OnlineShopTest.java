788fe7b94c75d630da926dfe70ff65ba5e952e34
Matias Nabarro <matiasnabarro@gmail.com>
2022-10-24 15:30:26 +0200

    test / implemented tests for findProductToViewDetails method

--- app/src/test/java/gradle/java/domain/OnlineShopTest.java
+++ app/src/test/java/gradle/java/domain/OnlineShopTest.java
@@ -1,27 +1,42 @@
 package gradle.java.domain;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.mockito.Mockito.when;
 
+import gradle.java.infraestructure.presentation.CatalogFormatter;
 import gradle.java.infraestructure.presentation.MenuMessages;
+import gradle.java.infraestructure.presentation.ProductFormatter;
+import java.util.Optional;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import resources.TestDatabase;
 
 class OnlineShopTest {
 
   private AutoCloseable closeable;
-  @Mock
+  TestDatabase database;
+  CatalogFormatter catalogFormatter;
+  ProductFormatter productFormatter;
+  StockRepository stockRepository;
   UserInterface userInterface;
 
   @BeforeEach
-  void setup () {
+  void setup() {
+
     closeable = MockitoAnnotations.openMocks(this);
+    database = new TestDatabase();
+    catalogFormatter = Mockito.mock(CatalogFormatter.class);
+    productFormatter = Mockito.mock(ProductFormatter.class);
+    stockRepository = Mockito.mock(StockRepository.class);
+    userInterface = Mockito.mock(UserInterface.class);
+
   }
 
   @AfterEach
-  void atLast () throws Exception {
+  void atLast() throws Exception {
     closeable.close();
   }
 
@@ -30,7 +45,26 @@ class OnlineShopTest {
   }
 
   @Test
-  void findProductToViewDetailsTest() {
-    when(userInterface.waitForUserInput(MenuMessages.whichProductToExplore)).thenReturn("W2C");
+  void findProductToViewDetailsWithExistingProductTest() {
+    String productReference = "W2C";
+
+    Optional<Product> expectedProduct = database.findByReference(productReference);
+    OnlineShop onlineShop = new OnlineShop(database, catalogFormatter, productFormatter, stockRepository, userInterface);
+
+    when(userInterface.waitForUserInput(MenuMessages.whichProductToExplore)).thenReturn(productReference);
+
+    assertThat(onlineShop.findProductToViewDetails()).usingRecursiveComparison().isEqualTo(expectedProduct);
+  }
+
+  @Test
+  void findProductToViewDetailsWithNonExistingProductTest() {
+
+    String nonExistingProductReference = "W5K";
+
+    OnlineShop onlineShop = new OnlineShop(database, catalogFormatter, productFormatter, stockRepository, userInterface);
+
+    when(userInterface.waitForUserInput(MenuMessages.whichProductToExplore)).thenReturn(nonExistingProductReference);
+
+    assertThat(onlineShop.findProductToViewDetails()).isEqualTo(Optional.empty());
   }
 }
